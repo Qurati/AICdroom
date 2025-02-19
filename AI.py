fd6ddbca2bfd355f4554ccfcfaa1e61844fcdb9e
Qurati <159145788+Qurati@users.noreply.github.com>
2025-02-19 23:54:27 +0300

    Добавлены файлы main и AI. В файле main написан тг бот, а в AI запрос по API к GPT. TODO: в main сделать только запуск кода, бота вынести в отдельный файл

---
+++ AI.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+
+client = OpenAI(
+  api_key="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"
+)
+
+def req(msg):
+    completion = client.chat.completions.create(
+  model="gpt-4o-mini",
+  store=True,
+  messages=[
+    {"role": "user", "content": f"{msg}"}
+  ]
+)
+    return completion.choices[0].message.content
+
+#print(req().choices[0].message.content)
---
+++ main.py
@@ -0,0 +1,27 @@
+from aiogram import Bot, Dispatcher, executor, types
+from AI import req
+
+API_TOKEN = '7983759358:AAHhLAi9Sboz1DL0d2Rls2d-1Fa8TfUHDIw'
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+  await message.reply(req(message.text))
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
