a4685ff316206c34eb63dee96818a96610f26dc3
Qurati <159145788+Qurati@users.noreply.github.com>
2025-02-20 17:57:30 +0300

    добавил proxy для работы на сервере

--- AI.py
+++ AI.py
@@ -1,17 +1,22 @@
 import openai
 
-client = openai.OpenAI(
-  api_key="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"
-)
+# Настройки прокси
+proxies = {
+    "http": "154.16.146.45:80",
+    "https": "177.234.209.82:999",
+}
+
+# Установите прокси для библиотеки requests
+openai.proxy = proxies
+openai.api_key = "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"
+
 
 def req(msg):
-    completion = client.chat.completions.create(
-  model="gpt-4o-mini",
-  store=True,
-  messages=[
-    {"role": "user", "content": f"{msg}"}
-  ]
-)
-    return completion.choices[0].message.content
+    response = openai.ChatCompletion.create(
+        model="gpt-4o-mini",
+        store=True,
+        messages=[{"role": "user", "content": f"{msg}"}],
+    )
+    return response.choices[0].message['content']
 
 #print(req('hi'))
