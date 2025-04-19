56cc74c876c8c314da431ddc8ca0582f2c442296
Qurati <159145788+Qurati@users.noreply.github.com>
2025-04-19 23:06:01 +0300

    скрыл ключи

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
-API_TOKEN = '7983759358:AAHhLAi9Sboz1DL0d2Rls2d-1Fa8TfUHDIw'
-openAI_key = "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"
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+openAI_key = os.getenv('openAI_key')
+API_TOKEN = os.getenv('API_TOKEN')
 GPT_models = ["GPT-3.5", "GPT-4", "GPT-4o-mini", "GPT-4-Turbo"]
 AI_models = ['Chat GPT', 'Yandex GPT', 'DeepSeek']
 
