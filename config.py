a8f85cf14e272b295c2a6289c0c23d49afce49f0
Qurati <159145788+Qurati@users.noreply.github.com>
2025-04-19 23:06:45 +0300

    скрыл ключи

--- config.py
+++ config.py
@@ -6,6 +6,3 @@ openAI_key = os.getenv('openAI_key')
 API_TOKEN = os.getenv('API_TOKEN')
 GPT_models = ["GPT-3.5", "GPT-4", "GPT-4o-mini", "GPT-4-Turbo"]
 AI_models = ['Chat GPT', 'Yandex GPT', 'DeepSeek']
-
-
-#sk-808f25e71a8348a48cd4a438b93eb989 DEEPSEEK
