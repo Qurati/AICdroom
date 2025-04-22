a7a0a5d98de407f87a7cfcde48f8b647c93a927c
Qurati <159145788+Qurati@users.noreply.github.com>
2025-04-22 14:10:40 +0300

    пофиксил баг с форматированием

--- AI/msg_format.py
+++ AI/msg_format.py
@@ -3,13 +3,12 @@ import re
 
 def escape_markdown(text):
     # Экранируем все спецсимволы, которые требуют защиты в MarkdownV2
-    return re.sub(r'([\[\]()~>#+\-=|{}.!])', r'\\\1', text)
+    return re.sub(r'([\[\]()~>#+\-=|{}.!\\])', r'\\\1', text)
 
+def escape_markdown_v2(text):
+    return re.sub(r'([\[\]()~>#+\-=|{}.!\\])', r'\\\1', text)
 
 def format_response(role_text, answer, ai_name):
-    ai_name = escape_markdown(ai_name)
-    role_text = escape_markdown(role_text)
-    answer = escape_markdown(answer)
     if "преподаватель" in role_text:
         role_line = f"📚 *Учитель ({ai_name}) отвечает:*"
     elif "психолог" in role_text:
--- handlers/anws_handler.py
+++ handlers/anws_handler.py
@@ -1,13 +1,15 @@
 from aiogram import types
 from AI.AI_func import req
+from AI.msg_format import *
+
 
 def start_answer(dp):
     @dp.message_handler(lambda msg: not msg.via_bot)
     async def process_question(message: types.Message):
         try:
-            await message.reply(req(message), parse_mode="MarkdownV2")
+            await message.reply(escape_markdown_v2(req(message)), parse_mode="MarkdownV2")
         except:
             try:
-                await message.reply(req(message), parse_mode="Markdown")
+                await message.reply(escape_markdown(req(message)), parse_mode="Markdown")
             except:
                 await message.reply(req(message))
