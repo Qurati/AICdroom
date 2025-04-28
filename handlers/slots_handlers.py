e1a89f48b94ac7d9f4c7cd7146553e69e8233c49
Qurati <159145788+Qurati@users.noreply.github.com>
2025-04-28 12:17:23 +0300

    рефакторинг

--- handlers/slots_handlers.py
+++ handlers/slots_handlers.py
@@ -18,72 +18,42 @@ def slots_handlers(dp):
         if message.text == 'Сохраненные чаты':
             await message.answer("Выберите слот:", reply_markup=slots_kb(user_id))
 
+
+    @dp.callback_query_handler(lambda c: c.data.startswith("slot_"))
+    async def slot_handler(call: types.CallbackQuery):
+        slot_id = int(call.data.split("_")[-1])
+        if slot_id == 1:
+            await call.message.edit_text(f"Действие для {get_slot_name(call.from_user.id, slot_id)}:", reply_markup = slot1)
+        if slot_id == 2:
+            await call.message.edit_text(f"Действие для {get_slot_name(call.from_user.id, slot_id)}:", reply_markup = slot2)
+        if slot_id == 3:
+            await call.message.edit_text(f"Действие для {get_slot_name(call.from_user.id, slot_id)}:", reply_markup = slot3)
+
+    @dp.callback_query_handler(lambda c: c.data.startswith("save_"))
+    async def save_slot(call: types.CallbackQuery, state: FSMContext):
+        slot_id = int(call.data.split("_")[-1])
+        save_context_to_slot(call.from_user.id, slot_id)
+        await call.answer(f'Ваша переписка сохранена в {get_slot_name(call.from_user.id, slot_id)}', True)
+
+    @dp.callback_query_handler(lambda c: c.data.startswith("load_"))
+    async def load_slot(call: types.CallbackQuery, state: FSMContext):
+        slot_id = int(call.data.split("_")[-1])
+        load_context_from_slot(call.from_user.id, slot_id)
+        await call.answer(f'Ваша переписка загружена из {get_slot_name(call.from_user.id, slot_id)}', True)
+
+    @dp.callback_query_handler(lambda c: c.data.startswith("delete_"))
+    async def load_slot(call: types.CallbackQuery, state: FSMContext):
+        slot_id = int(call.data.split("_")[-1])
+        clear_slot(call.from_user.id, slot_id)
+        await call.answer(f'Ваша переписка удалена из {get_slot_name(call.from_user.id, slot_id)}', True)
+
+
     @dp.callback_query_handler(text="return_btn")
     async def return_handler(call: types.CallbackQuery):
         user_id = call.from_user.id
         await call.message.edit_text("Выберите слот:", reply_markup=slots_kb(user_id))
 
-    #взаимодействие со слотом 1
-    @dp.callback_query_handler(text="slot1")
-    async def slot1_handler(call: types.CallbackQuery):
-        await call.message.edit_text(f"Действие для {get_slot_name(call.from_user.id, 1)}:", reply_markup=slot1_inl_kb)
-
-    @dp.callback_query_handler(text="save1")
-    async def save_slot1(call: types.CallbackQuery):
-        save_context_to_slot(call.from_user.id, 1)
-        await call.answer(f'Ваша переписка сохранена в {get_slot_name(call.from_user.id, 1)}', True)
-    @dp.callback_query_handler(text="load1")
-    async def load_slot1(call: types.CallbackQuery):
-        load_context_from_slot(call.from_user.id, 1)
-        await call.answer(f'Ваша переписка загружена из {get_slot_name(call.from_user.id, 1)}', True)
-
-    @dp.callback_query_handler(text="delete1")
-    async def delete_slot1(call: types.CallbackQuery):
-        clear_slot(call.from_user.id, 1)
-        await call.answer(f'Ваша переписка удалена из {get_slot_name(call.from_user.id, 1)}', True)
-
-    #взаимодействие со слотом 2
-    @dp.callback_query_handler(text="slot2")
-    async def slot1_handler(call: types.CallbackQuery):
-        await call.message.edit_text(f"Действие для {get_slot_name(call.from_user.id, 2)}:", reply_markup=slot2_inl_kb)
-
-    @dp.callback_query_handler(text="save2")
-    async def save_slot2(call: types.CallbackQuery):
-        save_context_to_slot(call.from_user.id, 2)
-        await call.answer(f'Ваша переписка сохранена в {get_slot_name(call.from_user.id, 2)}', True)
-
-    @dp.callback_query_handler(text="load2")
-    async def load_slot2(call: types.CallbackQuery):
-        load_context_from_slot(call.from_user.id, 2)
-        await call.answer(f'Ваша переписка загружена из {get_slot_name(call.from_user.id, 2)}', True)
-
-    @dp.callback_query_handler(text="delete2")
-    async def delete_slot2(call: types.CallbackQuery):
-        clear_slot(call.from_user.id, 2)
-        await call.answer(f'Ваша переписка удалена из {get_slot_name(call.from_user.id, 2)}', True)
-
-    #взаимодействие со слотом 3
-    @dp.callback_query_handler(text="slot3")
-    async def slot3_handler(call: types.CallbackQuery):
-        await call.message.edit_text(f"Действие для {get_slot_name(call.from_user.id, 3)}:", reply_markup=slot3_inl_kb)
-
-    @dp.callback_query_handler(text="save3")
-    async def save_slot3(call: types.CallbackQuery):
-        save_context_to_slot(call.from_user.id, 3)
-        await call.answer(f'Ваша переписка сохранена в {get_slot_name(call.from_user.id, 3)}', True)
-
-    @dp.callback_query_handler(text="load3")
-    async def load_slot3(call: types.CallbackQuery):
-        load_context_from_slot(call.from_user.id, 3)
-        await call.answer(f'Ваша переписка загружена из {get_slot_name(call.from_user.id, 3)}', True)
-
-    @dp.callback_query_handler(text="delete3")
-    async def delete_slot3(call: types.CallbackQuery):
-        clear_slot(call.from_user.id, 3)
-        await call.answer(f'Ваша переписка удалена из {get_slot_name(call.from_user.id, 3)}', True)
-
-
-#///////////////////////////////
+    #///////////////////////////////
     class RenameSlotState(StatesGroup):
         waiting_for_name = State()
 
--- kb.py
+++ kb.py
@@ -31,34 +31,34 @@ def get_subscription_kb(channel: str) -> InlineKeyboardMarkup:
 
 
 def slots_kb(user_id):
-    slot1 = InlineKeyboardButton(get_slot_name(user_id, 1), callback_data='slot1')
-    slot2 = InlineKeyboardButton(get_slot_name(user_id, 2), callback_data='slot2')
-    slot3 = InlineKeyboardButton(get_slot_name(user_id, 3), callback_data='slot3')
+    slot1 = InlineKeyboardButton(get_slot_name(user_id, 1), callback_data='slot_1')
+    slot2 = InlineKeyboardButton(get_slot_name(user_id, 2), callback_data='slot_2')
+    slot3 = InlineKeyboardButton(get_slot_name(user_id, 3), callback_data='slot_3')
     btn_list = [slot1, slot2, slot3]
     slots_num_kb = InlineKeyboardMarkup(row_width=1).add(*btn_list)
     return slots_num_kb
 return_btn = InlineKeyboardButton("Вернуться", callback_data='return_btn')
 
 rename_btn1 = InlineKeyboardButton('Переименовать слот', callback_data='rename_slot_1')
-save_btn1 = InlineKeyboardButton('Сохранить', callback_data='save1')
-load_btn1 = InlineKeyboardButton('Загрузить', callback_data='load1')
-delete_btn1 = InlineKeyboardButton('Удалить', callback_data='delete1')
+save_btn1 = InlineKeyboardButton('Сохранить', callback_data='save_1')
+load_btn1 = InlineKeyboardButton('Загрузить', callback_data='load_1')
+delete_btn1 = InlineKeyboardButton('Удалить', callback_data='delete_1')
 btn_list = [rename_btn1, save_btn1, load_btn1, delete_btn1, return_btn]
-slot1_inl_kb = InlineKeyboardMarkup(row_width=1).add(*btn_list)
+slot1 = InlineKeyboardMarkup(row_width=1).add(*btn_list)
 
 rename_btn2 = InlineKeyboardButton('Переименовать слот', callback_data='rename_slot_2')
-save_btn2 = InlineKeyboardButton('Сохранить', callback_data='save2')
-load_btn2 = InlineKeyboardButton('Загрузить', callback_data='load2')
-delete_btn2 = InlineKeyboardButton('Удалить', callback_data='delete2')
+save_btn2 = InlineKeyboardButton('Сохранить', callback_data='save_2')
+load_btn2 = InlineKeyboardButton('Загрузить', callback_data='load_2')
+delete_btn2 = InlineKeyboardButton('Удалить', callback_data='delete_2')
 btn_list = [rename_btn2, save_btn2, load_btn2, delete_btn2, return_btn]
-slot2_inl_kb = InlineKeyboardMarkup(row_width=1).add(*btn_list)
+slot2 = InlineKeyboardMarkup(row_width=1).add(*btn_list)
 
 rename_btn3 = InlineKeyboardButton('Переименовать слот', callback_data='rename_slot_3')
-save_btn3 = InlineKeyboardButton('Сохранить', callback_data='save3')
-load_btn3 = InlineKeyboardButton('Загрузить', callback_data='load3')
-delete_btn3 = InlineKeyboardButton('Удалить', callback_data='delete3')
+save_btn3 = InlineKeyboardButton('Сохранить', callback_data='save_3')
+load_btn3 = InlineKeyboardButton('Загрузить', callback_data='load_3')
+delete_btn3 = InlineKeyboardButton('Удалить', callback_data='delete_3')
 btn_list = [rename_btn3, save_btn3, load_btn3, delete_btn3, return_btn]
-slot3_inl_kb = InlineKeyboardMarkup(row_width=1).add(*btn_list)
+slot3 = InlineKeyboardMarkup(row_width=1).add(*btn_list)
 
 
 change_AI_kb = ReplyKeyboardMarkup(resize_keyboard=True)
